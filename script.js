7bf4654ce10964c967048cbfa434135cbdfd7327
MATHEUS LACERDA MACHADO DE SOUZA <matheus.lmsouza@senacsp.edu.br>
2026-05-14 20:02:23 -0300

    AGORA VAI

--- script.js
+++ script.js
@@ -105,9 +105,9 @@ function removerPedido(){
     const id = Number (document.getElementById("idRemover").value);
 
 
-    fecth("https://projeto-api-x4p9.onrender.com/pedidos",{
+    fetch("https://projeto-api-x4p9.onrender.com/pedidos",{
     method: "DELETE",
-    headers:{
+    headers: {
         'Content-Type': "application/json"
         },
         // envia apenas o id do pedido que sera removido 
